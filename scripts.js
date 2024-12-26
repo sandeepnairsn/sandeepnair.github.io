a67216b968e9bc07268bd2fd1e9a00f590c28af3
sandeepnairsn <sandeepnairs@gmail.com>
2024-12-26 12:47:23 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Function to scroll to a specific section when the user clicks a button
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
